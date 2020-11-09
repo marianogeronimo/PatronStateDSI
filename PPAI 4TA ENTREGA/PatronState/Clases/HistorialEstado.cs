3ec810762d84e43a88fddbb47e407e0eb814ba43
Julián Mansilla <julianm_98@hotmail.com>
2020-11-09 02:35:25 -0300

    HistorialEstado completo

--- PPAI 4TA ENTREGA/PatronState/Clases/HistorialEstado.cs
+++ PPAI 4TA ENTREGA/PatronState/Clases/HistorialEstado.cs
@@ -8,5 +8,25 @@ namespace PatronState.Clases
 {
     class HistorialEstado
     {
+        private DateTime fechaHoraFin;
+        private DateTime fechaHoraInicio;
+        private Estado estado;
+
+        public HistorialEstado(DateTime fechaHoraInicio, Estado estado)
+        {
+            this.fechaHoraInicio = fechaHoraInicio;
+            conocerEstado(estado);
+        }
+
+        public DateTime FechaHoraFin { get => fechaHoraFin; set => fechaHoraFin = value; }
+        public DateTime FechaHoraInicio { get => fechaHoraInicio; set => fechaHoraInicio = value; }
+        internal Estado Estado { get => estado; set => estado = value; }
+
+        public void conocerEstado(Estado estado)
+        {
+            this.estado = estado;
+        }
+
+
     }
 }
